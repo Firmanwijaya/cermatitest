5951d1018b11c6866398cb412586a29d3f7f47aa
unknown <firmanwijayadinata90@gmail.com>
2022-07-10 02:41:09 +0700

    not fix notification panel

---
+++ main.js
@@ -0,0 +1,11 @@
+// const button = document.querySelector(".gotit"),
+//   notification = document.querySelector(".notification");
+
+// button.addEventListener("click", () => {
+//   notification.classList.add("visually-hidden");
+// });
+
+function diklik() {
+  document.querySelector(".header").style.margin = "0";
+  document.querySelector(".notification").style.height = "0";
+}
